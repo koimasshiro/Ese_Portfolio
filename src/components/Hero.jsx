494636ab5b9f3ecafbe475bf8b5fc6a5b7947097
koimasshiro <miss.eselawrence@gmail.com>
2024-07-27 21:59:00 -0700

    style: update About, Experience, and Tech sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient"/>
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff] ">Eseosa Imhariagbe</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I am <span className="text-[#915eff] ">Eseosa Imhariagbe</span></h1>
         <p className={`${styles.heroSubText} mt-2 text-white-100`}>MERN Stack Developer | Web Developer</p>
         </div>
 
